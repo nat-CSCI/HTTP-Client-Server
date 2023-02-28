3027b36ed065ee7b64d9a5c9ac6d75f02ed26300
nat-CSCI <91428436+nat-CSCI@users.noreply.github.com>
2023-02-28 11:29:08 -0600

    h

--- npclient1.py
+++ npclient1.py
@@ -8,3 +8,10 @@ serverPort = 12000
 
 # Create UDP socket for server
 clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+message = 'GET index.html'
+
+clientSocket.sendto(message.encode(), (serverName, serverPort))
+
+receivedMessage, serverAddress = clientSocket.recvfrom(2048)
+print(receivedMessage)
