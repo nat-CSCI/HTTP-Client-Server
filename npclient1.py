eec0b71373798940bf244f6e25e98a947d3eed34
nat-CSCI <natalie.dykhuis@snc.edu>
2023-02-28 11:42:44 -0600

    getting file

--- npclient1.py
+++ npclient1.py
@@ -1,6 +1,5 @@
 # UDPclient.py
 from socket import * #include Python's socket library
-
 import sys
 
 serverName = '127.0.0.1' #hostname
@@ -8,3 +7,12 @@ serverPort = 12000
 
 # Create UDP socket for server
 clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+message = 'GET index.html'
+
+clientSocket.sendto(message.encode(), (serverName, serverPort))
+
+receivedMessage, serverAddress = clientSocket.recvfrom(2048)
+print(receivedMessage)
+
+f = open(receivedMessage)
--- npserver1.py
+++ npserver1.py
@@ -1,12 +1,21 @@
 # UDPserver
 from socket import *
+from os.path import exists
 import sys
 serverPort = 12000
 
 # Create UDP socket
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 
-playing = False
 
 # Bind socket to local port number 12000
 serverSocket.bind(('', serverPort))
+print('Ready to receive')
+
+while 1:
+    message, clientAddress = serverSocket.recvfrom(2048)
+    if "index.html" in message.decode():
+        path = "C:/Users/natdy/OneDrive/Desktop/HTTP Client Server/HTTP-Client-Server/SampleWebPage/index.html"
+        if exists(path):
+            newMessage = "index.html is there"
+            serverSocket.sendto(path.encode(),clientAddress)
