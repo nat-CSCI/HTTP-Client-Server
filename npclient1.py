5ddc9509cd99df250e385f246610689ebe1d3c69
mxbernerSNC <91630245+mxbernerSNC@users.noreply.github.com>
2023-03-07 03:07:53 -0600

    Framework
    Did a lot of commenting, clarifying, and framework for moving forward with the program

--- npclient1.py
+++ npclient1.py
@@ -1,21 +1,52 @@
 # UDPclient.py
 from socket import * #include Python's socket library
-import sys
 from html.parser import HTMLParser
+import sys
+import time
 
-serverName = '127.0.0.1' #hostname
-serverPort = 12000
 
-# Create UDP socket for server
-clientSocket = socket(AF_INET, SOCK_DGRAM)
+""" [Command Line Notes]
+Format of command line:
+    python myclient.py server_ip server_port file_name
 
+0. len(sys.argv) = 4
+1. sys.argv[0] = myclient
+2. sys.argv[1] = server_ip
+3. sys.argv[2] = server_port
+4. sys.argv[3] = file_name
+"""
+
+#########################
+#-------Setting up------#
+#########################
+serverName = '127.0.0.1' #set IP
+#serverName = sys.argv[1] #hostname with command line args
+serverPort = 12000 #set port
+#serverPort = int(sys.argv[2]) #set port with command line args
+clientSocket = socket(AF_INET, SOCK_DGRAM) # Create UDP socket for server
+start_time = time.time()
+
+
+##########################
+#-------GET Request------#
+##########################
 message = 'GET index.html'
-
 clientSocket.sendto(message.encode(), (serverName, serverPort))
-
 receivedMessage, serverAddress = clientSocket.recvfrom(2048)
+print(receivedMessage.decode())
 
-print(receivedMessage)
+##########################
+#-------HTML parser------#
+##########################
+""" [HTML Parser Notes]
+On a starttag img
+    send the message 'img' to server
+    send the request for referenced file
+    receive the referenced file
+On a endtag /html
+    send the message '/html'
+    end program
+"""
 
 class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
@@ -25,10 +56,11 @@ class MyHTMLParser(HTMLParser):
             print('Attribute: ', s[1])
             clientSocket.sendto(s[1].encode(), (serverName, serverPort))
 
-
     def handle_endtag(self, tag):
-         if tag == 'img':
+        if tag == 'img':
             print("Encountered an end tag :", tag)
+        elif tag == 'html':
+             print("Encountered an end tag :", tag)
 
     def handle_data(self, data):
             if data.__contains__("index_files"):
@@ -37,6 +69,25 @@ class MyHTMLParser(HTMLParser):
 parser = MyHTMLParser()
 
 f = open("index.html",encoding='utf8')
-parser.feed(f.read())
+parsedFile = parser.feed(f.read())
 
+"""[Rough code for handling message sending to client]
+for x in parsedFile:
+	if(x[0] == '/'): #end tag
+		serverSocket.sendto(x.encode(), clientAddress) #send the prompt to client
+		if(x == '%end'):
+			break
+		message, clientAddress = serverSocket.recvfrom(2048) #recieve response from client
+		modifiedMessage = message.decode()
+		updatedfiletext = updatedfiletext + modifiedMessage + " "
+	else: #start tag
+		updatedfiletext = updatedfiletext + x + " " #stores in updatedfiletext
 
+"""
+
+########################
+#----Finish Program----#
+########################
+elapsed_time = time.time() - start_time
+print('Elapsed Time: ' + elapsed_time)
+sys.exit("-----")
--- npserver1.py
+++ npserver1.py
@@ -2,21 +2,61 @@
 from socket import *
 from os.path import exists
 import sys
-serverPort = 12000
-
-# Create UDP socket
-serverSocket = socket(AF_INET, SOCK_DGRAM)
+import time
 
 
-# Bind socket to local port number 12000
-serverSocket.bind(('', serverPort))
+""" [Command Line Notes]
+Format of command line:
+    python myserver.py server_port directory_path
+
+0. len(sys.argv) = 3
+1. sys.argv[0] = myserver
+2. sys.argv[1] = server_port 
+3. sys.argv[2] = directory_path
+"""
+
+#########################
+#-------Setting up------#
+#########################
+serverPort = 12000 #Set serverPort
+#serverPort = int(sys.argv[1]) #Set serverPort with command line args
+serverSocket = socket(AF_INET, SOCK_DGRAM) # Create UDP socket
+serverSocket.bind(('', serverPort)) # Bind socket to local port number 12000
+#path = "C:/Users/natdy/OneDrive/Desktop/HTTP Client Server/HTTP-Client-Server/SampleWebPage/index.html"
+#path = sys.argv[2] #set the directory path
+start_time = time.time()
 print('Ready to receive')
 
+#################################
+#--Wait for Index.html request--#
+#################################
 while 1:
     message, clientAddress = serverSocket.recvfrom(2048)
     if "index.html" in message.decode():
-        # path = "C:/Users/natdy/OneDrive/Desktop/HTTP Client Server/HTTP-Client-Server/SampleWebPage/index.html"
         # if exists(path):
          newMessage = "Got it"
          serverSocket.sendto(newMessage.encode(),clientAddress )
+         break
  
+###############################
+#----Image/Source Requests----#
+###############################
+""" [Outline for recieving requests]
+while 1:
+	tag, serverAddress = clientSocket.recvfrom(2048)
+	tag = tag.decode()
+    print(tag)
+	
+	if(tag == '/html'): #exit on tag /html
+		break;
+    else if (tag == 'img') #handle image request on tag img
+        #find resource using path
+        #send resource to server
+"""
+
+########################
+#----Finish Program----#
+########################
+elapsed_time = time.time() - start_time
+print('Elapsed Time: ' + elapsed_time)
+sys.exit("-----")
