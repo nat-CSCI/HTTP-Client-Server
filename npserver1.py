a63f8e363192607dfcdd04d7016fc47e55676c1d
mxbernerSNC <91630245+mxbernerSNC@users.noreply.github.com>
2023-04-11 11:17:59 -0500

    silly changes

--- npserver1.py
+++ npserver1.py
@@ -26,11 +26,12 @@ Format of command line:
 2. sys.argv[1] = server_port 
 3. sys.argv[2] = directory_path
 """
-serverPort = 12000 #Set serverPort
+
+serverPort = 3904 #Set serverPort
 #serverPort = int(sys.argv[1]) #Set serverPort with command line args
 serverSocket = socket(AF_INET, SOCK_DGRAM) # Create UDP socket
 serverSocket.bind(('', serverPort)) # Bind socket to local port number 12000
-path = "C:/Users/natdy/OneDrive/Desktop/HTTP-Client-Server/HTTP-Client-Server"
+path = "/home/bernmt/csci373/"
 #path = sys.argv[2] #set the directory path
 start_time = time.time()
 print('Ready to receive')
